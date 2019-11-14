88e107fdc7ba9571fe10d19bcf412ae9385915dc
whitecat4810 <48373339+whitecat4810@users.noreply.github.com>
2019-11-14 08:24:02 +0800

    Add files via upload

---
+++ 04 模版/分页以及转账模版/src/com/aufe/service/LogService.java
@@ -0,0 +1,15 @@
+﻿package com.aufe.service;
+
+import java.io.IOException;
+
+import com.aufe.pojo.PageInfo;
+
+public interface LogService  {
+	/**
+	 * 分页显示
+	 * @param pageSize
+	 * @param pageNumber
+	 * @return
+	 */
+	PageInfo showPage(int pageSize,int pageNumber) throws IOException;
+}
